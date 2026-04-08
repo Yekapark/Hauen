2c977d4e6432b7d74d30cf39e7883d784d54df45
sotatek-loganpark <logan.park@sotatek.com>
2026-04-08 18:05:02 +0900

    디자인커밋

---
+++ src/main/java/com/hauen/HauenApplication.java
@@ -0,0 +1,13 @@
+package com.hauen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HauenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HauenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hauen/HauenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hauen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HauenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
